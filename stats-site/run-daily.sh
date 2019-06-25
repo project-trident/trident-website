1693de07b02e8e665b82ca729b45dbceb8911820
Ken Moore <ken@ixsystems.com>
2019-06-25 16:01:57 -0400

    Some more work on the stats-site scripts. Add a jenkinsfile for syncing the files up too

--- stats-site/run-daily.sh
+++ stats-site/run-daily.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 DIR=$(dirname $0)
-if [ ! -d "${DIR}/stats" ] ; then
-  mkdir -p "${DIR}/stats"
-fi
+STATSDIR="/root/stats-cache"     
+if [ ! -d "${STATSDIR}" ] ; then
+  mkdir -p "${STATSDIR}"
+fi  
 #First re-generate the current-month stats file
-${DIR}/stats-scan analyze ${DIR}/stats/currentmonth.json /var/log/nginx-stats.log
+${DIR}/stats-scan analyze ${STATSDIR}/currentmonth.json /var/log/nginx-stats.log
 #And now regenerate the daily/monthly graph files from the cache
-${DIR}/stats-scan graph ${DIR}/web/daily.json true ${DIR}/stats/currentmonth.json ${DIR}/stats/lastmonth.json
-${DIR}/stats-scan graph ${DIR}/web/monthly.json false ${DIR}/stats/*.json
+${DIR}/stats-scan graph ${DIR}/web/daily.json true ${STATSDIR}/currentmonth.json ${STATSDIR}/lastmonth.json
+${DIR}/stats-scan graph ${DIR}/web/monthly.json false ${STATSDIR}/*.json
--- stats-site/run-monthly.sh
+++ stats-site/run-monthly.sh
@@ -1,16 +1,17 @@
 #!/bin/sh                             
-DIR=$(dirname $0)          
-if [ ! -d "${DIR}/stats" ] ; then
-  mkdir -p "${DIR}/stats"
+DIR=$(dirname $0)   
+STATSDIR="/root/stats-cache"       
+if [ ! -d "${STATSDIR}" ] ; then
+  mkdir -p "${STATSDIR}"
 fi           
 for log in `ls /var/log/nginx-stats.log.*`
 do 
   dname=`stat -f "%Sm" -t "%y-%m" ${log}`
-  dname="${DIR}/stats/${dname}.json"
+  dname="${STATSDIR}/${dname}.json"
   if [ -e "${dname}" ] ; then continue ; fi
   ${DIR}/stats-scan analyze "${dname}" /var/log/nginx-stats.log*
   if [ "${log}" = "/var/log/nginx-stats.log.0.bz2" ] ; then
     #Update the symlink for the most recent full-month
-    ln -sf "${dname}" "${DIR}/stats/lastmonth.json"
+    ln -sf "${dname}" "${STATSDIR}/lastmonth.json"
   fi
 done
